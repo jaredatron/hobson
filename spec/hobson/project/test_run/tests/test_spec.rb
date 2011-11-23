8b395c6a79cfcb28fa620c3d8af3c173993dbb68
Jared Grippe <techops+jared@change.org>
2011-11-22 17:21:10 -0800

    wip

--- spec/hobson/project/test_run/tests/test_spec.rb
+++ spec/hobson/project/test_run/tests/test_spec.rb
@@ -5,9 +5,13 @@ describe Hobson::Project::TestRun::Tests::Test do
   subject{ Factory.test }
   alias_method :test, :subject
 
-  %w{job status result runtime est_runtime}.each do |attr|
-    it { should respond_to :"#{attr}"  }
-    it { should respond_to :"#{attr}=" }
+  worker_context do
+
+    %w{job status result runtime est_runtime}.each do |attr|
+      it { should respond_to :"#{attr}"  }
+      it { should respond_to :"#{attr}=" }
+    end
+
   end
 
 end

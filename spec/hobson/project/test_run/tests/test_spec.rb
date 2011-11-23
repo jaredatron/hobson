e374b47b9688473d1e594ab145b14d0f085ade69
Jared Grippe <techops+jared@change.org>
2011-11-22 17:48:34 -0800

    wip

--- spec/hobson/project/test_run/tests/test_spec.rb
+++ spec/hobson/project/test_run/tests/test_spec.rb
@@ -14,13 +14,25 @@ describe Hobson::Project::TestRun::Tests::Test do
 
     describe "calculate_estimated_runtime!" do
 
-      it "should calculate nil" do
-        test.est_runtime.should be_nil
-        test.calculate_estimated_runtime!
-        test.est_runtime.should be_nil
+      context "when this test has never been run before" do
+        it "should calculate nil" do
+          test.est_runtime.should be_nil
+          test.calculate_estimated_runtime!
+          test.est_runtime.should be_nil
+        end
       end
 
-
+      context "when this test has been run before" do
+        before{
+          runtimes = 10.times.map{ rand }
+          10.times{ |i| Factory.test('factoried.feature').runtime = runtimes[i] }
+        }
+        it "should calculate the average ex" do
+          test.est_runtime.should be_nil
+          test.calculate_estimated_runtime!
+          test.est_runtime.should runtimes.inject(&:+) / runtimes.size
+        end
+      end
 
     end
 

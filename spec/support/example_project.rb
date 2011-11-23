987e26023c5ff4e98b4f333fb2c0487ef531a982
Jared Grippe <techops+jared@change.org>
2011-11-23 11:27:12 -0800

    WIP

--- spec/support/example_project.rb
+++ spec/support/example_project.rb
@@ -1,6 +1,7 @@
 module ExampleProject
 
   NAME    = 'example_hobson_project'
-  GIT_URL = 'git://github.com/deadlyicon/example_hobson_project.git'
+  GIT_URL = '/Users/deadlyicon/Work/change.org/GIT_CLONES/example_hobson_project'
+  # GIT_URL = 'git://github.com/deadlyicon/example_hobson_project.git'
 
 end

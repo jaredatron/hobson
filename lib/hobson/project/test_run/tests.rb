dcabf0e087cd5b739ff0c610c8192fb4ba87a6a1
Jared Grippe <techops+jared@change.org>
2011-11-22 17:08:51 -0800

    wip

--- lib/hobson/project/test_run/tests.rb
+++ lib/hobson/project/test_run/tests.rb
@@ -13,34 +13,30 @@ class Hobson::Project::TestRun::Tests
 
   delegate :each, :inspect, :to_s, :==, '<=>', :size, :length, :count, :to => :tests
 
-  def push *names
-    names.find_all(&:present?).each{|name| self[name] }
-  end
+  # def push *names
+  #   names.find_all(&:present?).each{|name| self[name] }
+  # end
 
-  def << *tests
-    tests.flatten.each{|test| push test }
-  end
+  # def << *tests
+  #   tests.flatten.each{|test| push test }
+  # end
 
-  def [] name
-    Test.new(self, name.to_s)
-  end
+  # def [] name
+  #   Test.new(self, name.to_s)
+  # end
 
-  def calculate_estimated_runtimes!
-    tests.each(&:calculate_estimated_runtime!)
-  end
+  # def calculate_estimated_runtimes!
+  #   tests.each(&:calculate_estimated_runtime!)
+  # end
 
-  def other_tests
-    @other_tests ||= (test_run.project.test_runs - test_run).map(&:tests)
-  end
+  # def other_tests
+  #   @other_tests ||= (test_run.project.test_runs - test_run).map(&:tests)
+  # end
 
   private
 
   def tests
-    data.
-      inject([]){ |tests, (key, value)| key =~ /^test:(.*):(.*)$/ and tests << $1; tests }.
-      uniq.
-      sort.
-      map{|name| self[name] }
+    @tests ||= test_run.workspace.tests.map{|name| Test.new(self, name) }
   end
 
 end

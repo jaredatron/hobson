bf67b23fe19f6fc057e615c91e3b2d7f08583747
Jared Grippe <techops+jared@change.org>
2012-01-09 19:30:08 -0800

    test_needing_to_be_run are not any tests that have not passed or been tried upto the limit

--- lib/hobson/project/test_run/job.rb
+++ lib/hobson/project/test_run/job.rb
@@ -12,7 +12,7 @@ class Hobson::Project::TestRun::Job
   end
 
   def test_needing_to_be_run
-    tests.find_all{|test| (test.waiting? || test.fail?) && test.tries <= Hobson.config[:max_retries] }
+    tests.find_all{|test| !test.pass? && test.tries <= Hobson.config[:max_retries] }
   end
 
   def est_runtime

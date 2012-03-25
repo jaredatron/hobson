632a8353db55bc47dfab4a6b3eb99ba546a119a9
Jared Grippe <jared@deadlyicon.com>
2012-03-24 17:14:10 -0700

    removed uneeded additional project_ref.needs_test_run? statement

--- lib/hobson/server.rb
+++ lib/hobson/server.rb
@@ -84,7 +84,6 @@ class Hobson::Server < Sinatra::Base
     origin_url.gsub!('&#x2F;','/') # I have no idea why i need this now
     project_ref = Hobson::CI::ProjectRef.new(origin_url, ref)
     project_ref.save
-    project_ref.run_tests! if project_ref.needs_test_run?
     redirect '/ci'
   end
 

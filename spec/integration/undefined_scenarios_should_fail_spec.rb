2abe457f4cbc0e982ae9e9768f5718f623f22e70
Jared Grippe <tech_ops+jared@change.org>
2012-05-09 11:56:15 -0700

    added an integration test to make sure hobson considers a scenario to be a failure when it has a set that is undefined

--- spec/integration/undefined_scenarios_should_fail_spec.rb
+++ spec/integration/undefined_scenarios_should_fail_spec.rb
@@ -3,12 +3,25 @@ require 'spec_helper'
 describe "undefined scenarios should fail spec" do
 
   let(:project) { Factory.project }
-  let(:test_run){ Factory.test_run(project, 'origin/testing_missing_step_definition') }
 
   worker_context do
-    test_run.build!
-    test_run.jobs.each(&:run_tests!)
-    debugger;1
+
+    context "when running a scenario with an undefined step" do
+      it "should fail" do
+        Resque.stub(:workers).and_return(stub(:length => 2))
+        test_run = project.run_tests!('origin/testing_missing_step_definition', 'rspec')
+        Resque.run!
+        Resque.run!
+        tests = project.test_runs.last.tests
+        tests.map{|t| [t.name,t.result] }.to_set.should == Set[
+          ["This schenario should pass", "PASS"],
+          ["This schenario should fail because it has a missing step definition", "FAIL"],
+          ["As an admin I should be able to create a post and delete it", "PASS"],
+          ["As an admin I should be able to create a post", "PASS"],
+        ]
+      end
+    end
+
   end
 
 end

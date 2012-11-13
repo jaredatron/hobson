19786874bb1f2127ac94d2fda7a05519e0b4732b
Chase Stubblefield <chasestubblefield@gmail.com>
2012-11-13 12:09:53 -0800

    Fix encoding

--- lib/hobson/encoding.rb
+++ lib/hobson/encoding.rb
@@ -1,6 +1,6 @@
 module Hobson
   def self.reset_encoding!
-    # Encoding.default_internal = (Hobson.config[:encoding].try(:[], :internal) || 'UTF-8' rescue 'UTF-8')
+    Encoding.default_internal = (Hobson.config[:encoding].try(:[], :internal) || 'UTF-8' rescue 'UTF-8')
     Encoding.default_external = (Hobson.config[:encoding].try(:[], :external) || 'UTF-8' rescue 'UTF-8')
   end
 end
